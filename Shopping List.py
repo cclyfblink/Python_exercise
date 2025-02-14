2a1522d265ff7b97bf8fa42191bc6b78a856dec1
Yifan Lu <pplyfpp@gmail.com>
2025-02-14 17:25:06 -0500

    Create Shopping List.py

---
+++ Shopping List.py
@@ -0,0 +1,12 @@
+shopping_list = [
+    "Double Cream",
+    "Single Cream",
+    "Eggs",
+    "Oreos"
+]
+print("MY HEALTHY SHOPPING LIST")
+for item in shopping_list:
+    print(item)
+
+shopping_list[3] = "Vanilla Essence"
+print(shopping_list)
