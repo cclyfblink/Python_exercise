f2384957bf933bf746029588f264a62089d6cfb0
Yifan Lu <pplyfpp@gmail.com>
2025-02-14 17:16:09 -0500

    Create The meaning of life.py

---
+++ The meaning of life.py
@@ -0,0 +1,10 @@
+# Get the name of the user
+name = str(input("What is your name? "))
+
+# Ask for the meaning of life
+mol = str(input("Hello " + name + ", what is the meaning of life?"))
+
+# Print summary of results
+print("\nSUMMARY OF RESULTS")
+print(" \nName ==> "+ name)
+print(" \nMeaning of life ==> " + mol + "\n")
